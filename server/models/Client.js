63343617fa7e06ae4b3239ae678f58c8ca74a684
MohauKhakhu <khakhumohau@gmail.com>
2025-06-23 15:28:44 +0200

    new

---
+++ server/models/Client.js
@@ -0,0 +1,35 @@
+// server/models/Client.js
+const mongoose = require('mongoose');
+
+const ClientSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zip: String
+  },
+  notes: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Client', ClientSchema);
